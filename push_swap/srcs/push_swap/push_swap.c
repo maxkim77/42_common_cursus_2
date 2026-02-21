d1c8a64fce17436947569a6ac68b2732e3fc4066
maxkim77 <maxkim7723@gmail.com>
2026-02-22 01:39:21 +0900

    add

--- push_swap/srcs/push_swap/push_swap.c
+++ push_swap/srcs/push_swap/push_swap.c
@@ -20,7 +20,7 @@ static void	process_sort(t_stack_node **a, t_stack_node **b)
 		return ;
 	len = stack_len(*a);
 	if (len == 2)
-		sa(a, false);
+		sa(a, true);
 	else if (len == 3)
 		sort_three(a);
 	else
