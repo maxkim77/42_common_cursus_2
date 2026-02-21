f17e5d55584efc415d77ad3fdc82bc2c211ce5c1
maxkim77 <maxkim7723@gmail.com>
2026-02-22 02:05:03 +0900

    add

--- push_swap/srcs/push_swap/push_swap.c
+++ push_swap/srcs/push_swap/push_swap.c
@@ -39,7 +39,7 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		return (1);
 	if (argc == 2 && !argv[1][0])
-		free(&a);
+		free_errors(&a);
 	if (argc == 2)
 	{
 		new_argv = split(argv[1], ' ');
